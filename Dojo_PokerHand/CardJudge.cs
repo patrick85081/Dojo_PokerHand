45171979ec6f13b945881be7ed3c9e534f845f42
Patrick <patrick85081@gmail.com>
2019-01-13 20:33:22 +0800

    Add CardJudge StraightFlush Test

---
+++ Dojo_PokerHand/CardJudge.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Dojo_PokerHand
+{
+    public class CardJudge
+    {
+        private IReadOnlyList<Card> cards;
+
+        public CardJudge(string cardString)
+        {
+            cards = cardString.Split(',')
+                .Select(s => new Card(s))
+                .ToList()
+                .AsReadOnly();
+            Judge(cards);
+        }
+
+        private void Judge(IEnumerable<Card> cards)
+        {
+            if (IsFlush(cards) && IsStraight(cards))
+            {
+                CardType = CardType.StraightFlush;
+            }
+        }
+
+        private bool IsStraight(IEnumerable<Card> cards)
+        {
+            var isStraight = cards.GroupBy(c => c.Number).Count() == 5 &&
+                             cards.Max(c => c.Number) - cards.Min(c => c.Number) == 4;
+            return isStraight;
+        }
+
+        private bool IsFlush(IEnumerable<Card> cards)
+        {
+            var isFlush = cards.GroupBy(c => c.Suit).Count() == 1;
+            return isFlush;
+        }
+
+        public CardType CardType { get; private set; }
+    }
+}
---
+++ Dojo_PokerHand/CardJudgeTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Dojo_PokerHand
+{
+    [TestClass]
+    public class CardJudgeTest
+    {
+        [TestMethod]
+        public void StraightFlush()
+        {
+            var cardJudge = new CardJudge("H4,H6,H5,H7,H8");
+            Assert.AreEqual(CardType.StraightFlush, cardJudge.CardType);
+        }
+    }
+}
